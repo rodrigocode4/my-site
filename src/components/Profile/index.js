c8a81a50c99e1ebdf6cca9c2efb00954435cb6b3
Rodrigo Oliveira <rodrigocode4@gmail.com>
2019-11-17 16:34:29 -0300

    Components Organization; add styled-components; style component separation

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby'
+import Avatar from '../Avatar';
 
 const Profile = () => {
-    const {
-        site: {
-            siteMetadata: {
-                author, position, description
-            }
-        }
-    } = useStaticQuery(graphql`
+  const {
+    site: {
+      siteMetadata: {
+        author, position, description
+      }
+    }
+  } = useStaticQuery(graphql`
     query MysiteMetadata {
         site {
           siteMetadata {
@@ -21,39 +22,14 @@ const Profile = () => {
       
     `)
 
-    return (
-        <div className="profile-wrapper">
-            <h1>{author}</h1>
-            <h2>{position}</h2>
-            <p>{description}</p>
-        </div>
-    )
+  return (
+    <div className="profile-wrapper">
+      <Avatar />
+      <h1>{author}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
 }
-/* 
-const Profile = () => (
-    <StaticQuery
-        query={graphql`
-            query MysiteMetadata {
-                site {
-                  siteMetadata {
-                    author
-                    position
-                    description
-                  }
-                }
-              }
-              
-            `
-        }
-        render={({ site: { siteMetadata: { author, position, description } } }) => (
-            <div className="profile-wrapper">
-                <h1>{author}</h1>
-                <h2>{position}</h2>
-                <p>{description}</p>
-            </div>
-        )}
-    />
-
-) */
 
 export default Profile;
