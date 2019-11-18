409b0f47cedb4eb9b18cf5b8373bfc5b2c79d5a6
Rodrigo Oliveira <rodrigocode4@gmail.com>
2019-11-18 11:04:30 -0300

    add MenuLinks component

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Profile from '../Profile';
 import SocialLink from '../SocialLink';
 import * as S from './styled';
+import MenuLinks from '../MenuLinks';
 
 const Sidebar = () => (
     <S.SidebarWrapper>
         <Profile />
         <SocialLink />
+        <MenuLinks />
     </S.SidebarWrapper>
 )
 
