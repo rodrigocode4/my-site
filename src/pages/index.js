e82eb43a20efccf8043e22793a85a60e99b58871
Rodrigo Oliveira <rodrigocode4@gmail.com>
2019-11-21 19:48:19 -0300

    add posts in markdown and gatsby-transformer-remark

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,65 @@
 import React from "react"
-import { Link } from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import PostItem from "../components/PostItem"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Olá, gatsby</h1>
-    <Link to="about">sobre</Link>
-  </Layout>
-)
+const IndexPage = () => {
+	const { allMarkdownRemark } = useStaticQuery(graphql`
+		query PostList {
+			allMarkdownRemark {
+				edges {
+					node {
+							frontmatter {
+								background
+								category
+								date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+								title
+								description
+								color
+							}
+							timeToRead
+					}
+				}
+			}
+		}
+	`)
+
+	const PostList = allMarkdownRemark.edges
+
+	return (
+		<Layout>
+			<SEO title="Home" />
+			{
+				PostList.map(
+					({ node: {
+						frontmatter: {
+							background,
+							category,
+							date,
+							title,
+							description,
+							color
+						},
+						timeToRead
+					},
+					}) => (
+							<PostItem
+								slug="/about/"
+								background={background}
+								color={color}
+								category={category}
+								date={date}
+								timeToRead={timeToRead}
+								title={title}
+								description={description}
+							/>
+						))
+			}
+
+		</Layout>
+	)
+}
 
 export default IndexPage
