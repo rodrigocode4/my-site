7a9d8613d7b32ae808d36cbb6b9694a7a9e00c8e
Rodrigo Oliveira <rodrigocode4@gmail.com>
2020-03-27 16:12:01 -0300

    config algolia in gatsby

---
+++ src/utils/algoliaQueries.js
@@ -0,0 +1,42 @@
+const postsQuery = `{
+    posts: allMarkdownRemark(
+        sort: {fields: frontmatter___date, order: DESC}
+    ) {
+        edges {
+            node {
+                    objectID: id
+                    frontmatter {
+                        category
+                        date_timestemp: date
+                        title
+                        description
+                    }
+                    fields {
+                        slug
+                    }
+                    excerpt(pruneLength: 5000)
+             }
+        }
+    }
+}`
+
+const flatten = arr => arr.map(({ node: { frontmatter, ...rest }}) => ({
+    ...frontmatter,
+    date_timestemp: parseInt(
+        (new Date(frontmatter.date_timestemp).getTime() / 1000).toFixed(0)
+    ),
+    ...rest
+}))
+
+const queries = [
+    {
+      query: postsQuery,
+      transformer: ({ data }) => flatten(data.posts.edges), // optional
+      indexName: 'Posts',
+      settings: {
+        attributesToSnippets: ['excerpt:20']
+      }
+    },
+];
+
+module.exports = queries
